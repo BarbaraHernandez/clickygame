1a16efd33875e8f78fba0ccc201e1f523e63b204
BarbaraHernandez <bandezanne@gmail.com>
2019-01-23 21:36:59 -0500

    Updates

---
+++ precache-manifest.ddda468562f71510446e6df665127eb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "659827566e63bd204c9c",
+    "url": "/clickygame/static/js/main.65982756.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickygame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "659827566e63bd204c9c",
+    "url": "/clickygame/static/css/main.37020db6.chunk.css"
+  },
+  {
+    "revision": "f9619cd4367ee3b699cdeb103e465aef",
+    "url": "/clickygame/index.html"
+  }
+];
